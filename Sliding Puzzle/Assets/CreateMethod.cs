c712ec2e9e7b54abda25f24421273ec3d27fb2f1
BernardoLv <contact@bernardolimon.com>
2018-05-02 22:49:05 -0500

    Method Scene now generates maps

--- Sliding Puzzle/Assets/CreateMethod.cs
+++ Sliding Puzzle/Assets/CreateMethod.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class CreateMethod : MonoBehaviour {
 	public static string [,] generatedmap = new string [8,8];
-
+	public static int icenextcounter;
+	public static List<Vector2> doorable = new List<Vector2>();
 	// Use this for initialization
 	void Start () {
-		
+		Createfourbyfour();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	public static void Createfourbyfour(){
+	public void Createfourbyfour(){
+		ResetColor();
 		CreateIce();
+		Add2Outerwalls();
+		PopulateDoorPool();
+		AssignGoalAndStart();
+		PrintMap();
+		DrawMap();
 
 	}
 	public static void CreateIce(){
@@ -27,7 +34,120 @@ public class CreateMethod : MonoBehaviour {
 	}
 	public static void Add2Outerwalls(){
 		for(int i = 0; i<8; i++){
+			int var;
+			if(i<2 || i>5){
+				var = 0;
+			}
+			else{
+				var = 1;
+			}
+			switch(var){
+				case 0:
+					for(int j = 0; j<8; j++){
+						generatedmap[j,i] = "Wall";
+					}
+					break;
+				case 1:
+					for(int j = 0; j<8; j++){
+						if(j<2 || j>5){
+							generatedmap[j,i] = "Wall";
+						}
+						else{
+						}
+					}
+					break;
+				default:
+					break;
+			}
+		}
+	}
+	public static void PopulateDoorPool(){
+		for(int i = 0; i<8; i++){
+			for(int j = 0; j<8; j++){
+				if(generatedmap[j,i] == "Wall"){
+					CheckSides(j,i);
+				}
+			}
+		}
+	}
+	public static void CheckSides(int x, int y){
+		icenextcounter = 0;
+		GetWallTag(x-1,y);
+		GetWallTag(x+1,y);
+		GetWallTag(x,y+1);
+		GetWallTag(x,y-1);
+		if(icenextcounter>0){
+			Debug.Log("Can Place door at" + x + "+" + y);
+			int myx = x;
+			int myy = y;
+			doorable.Add(new Vector2(myx,myy));
+		}
+	}
+	public static void AssignGoalAndStart(){
+		int max = doorable.Count;
+		int num = Random.Range(0,max-1);
+		Vector2 Goal = doorable[num];
+		generatedmap[Mathf.RoundToInt(Goal.x),Mathf.RoundToInt(Goal.y)] = "Goal";
+		doorable.Remove(Goal);
+		max = doorable.Count;
+		num = Random.Range(1,max-1);
+		Vector2 Start = doorable[num];
+		generatedmap[Mathf.RoundToInt(Start.x), Mathf.RoundToInt(Start.y)] = "Start";
+	}
+	public static void GetWallTag(int x, int y){
+		if (x > 7 || x < 0){
 
+		}
+		else if(y>7 || y < 0){
+
+		}
+		else if (generatedmap[x,y] == "Ice"){
+			icenextcounter++;
+		}
+		else{
+
+		}
+	}
+	public static void PrintMap(){
+		for(int i = 0; i<8; i++){
+			for(int j = 0; j<8; j++){
+				Debug.Log(generatedmap[j,i]);
+			}
+		}
+	}
+	public static void DrawMap(){
+		for(int i = 0; i<8; i++){
+			for(int j = 0; j<8; j++){
+				
+				PaintTag(j,i);
+			}
+		}
+	}
+	public static void PaintTag(int x, int y){
+		string tag = generatedmap[x,y];
+		int myx = x;
+		int myy = -y;
+		Vector3 tiletotest = new Vector3(myx, myy, 0);
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(tiletotest, .1f);
+		if(colliders.Length == 0){
+
+		}
+		else{
+			foreach(Collider2D component in colliders){
+				if(tag == "Wall"){
+					component.GetComponent<SpriteRenderer>().color = Color.black;
+				}
+				else{
+
+				}
+			}
+		}
+	}
+	public static void ResetColor(){
+		GameObject[] icetiles;
+		icetiles = GameObject.FindGameObjectsWithTag("Ground");
+		foreach(GameObject icetile in icetiles ){
+			icetile.GetComponent<SpriteRenderer>().color = Color.white;
 		}
 	}
 }
